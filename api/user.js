6af7e3413b274bff9474808953243ca33ba848aa
FenixLuciferum <a.r.frisari@gmail.com>
2024-10-07 14:32:38 +0200

    Update user.js

--- api/user.js
+++ api/user.js
@@ -36,13 +36,6 @@ transporter.verify((error, success) => {
     }
 });
 
-router.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-
-
 //signup
 router.post('/signup', cors(corsOptions),  (req, res) => {
     let { username, email, password } = req.body;
