9f7ccbc3d761fdd1f850e95d4a96aa58efda13f6
FenixLuciferum <a.r.frisari@gmail.com>
2024-10-07 14:43:28 +0200

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("DB Connected");
 }).catch((err) => console.log(err));
